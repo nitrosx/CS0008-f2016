4c94bedf3810eb651533249cd53f7cf23225c1a1
nitrosx (Max Novelli) <man8@pitt.edu>
2016-09-05 09:52:46 -0400

    Executed Exercise 3 from Chapter 1.

---
+++ Ch1-Ex3/ch1-ex3.py
@@ -0,0 +1,16 @@
+#
+# Template for code submission
+# name       : My name goes here
+# email      : my email goes here
+# date       :
+# class      : CS0008-f2016
+# instructor : Max Novelli (man8@pitt.edu)
+#
+# Description:
+# description of this file goes here
+# Example: Starting with Python, Chapter 1, Exercise 3
+#
+# Notes:
+# any notes to the instructor and/or TA goes here
+
+# ...and now let's program with Python
